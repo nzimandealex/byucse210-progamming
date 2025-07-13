6b3cf6d8fc0cac5c5bae98ea290b17d3abe31859
nzimandepa@gmail.com <nzimandepa@gmail.com>
2025-07-13 15:13:36 +0200

    Exercise 2 & 3 are completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,78 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        // Console.Write("What is your grade percentage? ");
+        // string userGrade = Console.ReadLine();
+
+        // int grade = int.Parse(userGrade);
+
+        //if (grade >= 90)
+        //{
+        //    Console.WriteLine("A");
+        //}
+
+        //else if (grade >= 80 && grade < 90)
+        //{
+        //    Console.WriteLine("B");
+        //}
+
+        //else if (grade >= 70 && grade < 80)
+        //{
+        //   Console.WriteLine("C");
+        //}
+
+        //else if (grade >= 60 && grade < 70)
+        //{
+        //   Console.WriteLine("D");
+        //}
+
+        //else
+        //{
+        //    Console.WriteLine("F");
+
+        //}
+
+
+        //if (grade >= 70)
+        //{
+        //    Console.WriteLine("Course passed, Well Done!");
+        //}
+
+        //else
+        // {
+        //     Console.WriteLine("Course failed, try again.");
+        // }
+
+        Console.WriteLine("What is your grade? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Console.Write("What is the magic number? ");
+        //int magicNum = int.Parse(Console.ReadLine());
+
+        Random randonGenerator = new Random();
+        int magicNum = randonGenerator.Next(1, 101);
+
+        int guess = -1;
+        do
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (guess > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+
+        while (guess != magicNum);
+
+        Console.WriteLine("You guessed it!");
+        
+        
+        
+
+
     }
 }
